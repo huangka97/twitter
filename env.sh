04bf7f7f68d918f703d6c5da1a76152433692752
Karl Huang <karl.huang2009@gmail.com>
2018-07-17 14:11:52 -0700

    first commit

---
+++ env.sh
@@ -0,0 +1,1 @@
+export MONGODB_URI="mongodb://huangka97:Geonilkim97!@ds139951.mlab.com:39951/twitter-horizons"
---
+++ routes/index.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var router = express.Router();
+var models = require('../models/models');
+var User = models.User;
+var Follow = models.Follow;
+var Tweet = models.Tweet;
+
+// THE WALL - anything routes below this are protected by our passport (user must be logged in to access these routes)!
+router.use(function(req, res, next){
+  if (!req.user) {
+    res.redirect('/login');
+  } else {
+    return next();
+  }
+});
+
+router.get('/', function(req, res) {
+  res.send("Success! You are logged in.");
+});
+
+router.get('/users/', function(req, res, next) {
+
+
+  // Gets all users
+
+});
+
+
+router.get('/users/:userId', function(req, res, next) {
+
+  // Gets all information about a single user
+  models.User.findById(req.params.userId,function(err,user){
+    if(err){
+      console.log(err);
+      res.send(err);
+    }else{
+      res.render('singleProfile',{
+        user:user
+      })
+    }
+  })
+});
+
+
+  // Creates a follower
+
+router.post('/users/:userId/follow', function(req, res) {
+
+  // Create a new follower
+});
+
+
+router.get('/tweets/', function(req, res, next) {
+
+  // Displays all tweets in the DB
+
+});
+
+
+router.post('/tweets/:tweetId/likes', function(req, res, next) {
+
+  //Should add the current user to the selected tweets like list (a.k.a like the tweet)
+
+});
+
+
+router.get('/tweets/new', function(req, res, next) {
+
+  //Display the form to fill out for a new tweet
+
+});
+
+
+router.post('/tweets/new', function(req, res, next) {
+
+  // Handle submission of new tweet form, should add tweet to DB
+
+
+});
+
+module.exports = router;
